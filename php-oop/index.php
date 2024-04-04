d90efb3cf04fb2da2938e27c36a9c40989fc0bc7
yogapradnyana01 <163377392+yogapradnyana01@users.noreply.github.com>
2024-04-04 14:51:37 +0800

    Update index.php

--- php-oop/index.php
+++ php-oop/index.php
@@ -1,15 +1,23 @@
-<php
-include 'Pengguna.php';
-include 'Dosen.php';
-include 'Mahasiswa.php';
+<?php
+require 'Pengguna.php';
+require 'Dosen.php';
+require 'Mahasiswa.php';
 
-$pengguna =new pengguna();
-$pengguna->perkenalan("wayan");
+// Membuat objek dari kelas pengguna()
+$pengguna = new pengguna("adi",);
+$pengguna = new pengguna("arya");
+$pengguna->perkenalan();
 
-$Dosen_1 = new Dosen();
-$Dosen->perkenalan("Made");
+// Membuat objek dari kelas dosen()
+$Dosen_1 = new Dosen("made", "0822048001", "Backend Web Development");
+$Dosen_1 = new Dosen("susi","0820029005", "Rekayasa Perangkat Lunak");
+$Dosen_1->perkenalan();
+$Dosen_1->getMatakuliah();
 
-$Mahasiswa_1 = new Mahasiswa();
-$Mahasiswa_1->perkenalan("Komang");
+// Membuat objek dari kelas Mahasiswa()
+$Mahasiswa_1 = new Mahasiswa("Komang", "200010111", 24);
+$Mahasiswa_1 = new Mahasiswa("arya", "200030272", 18);
+$Mahasiswa_1->perkenalan();
+$Mahasiswa_1->getjumlahSKS();
 
 
